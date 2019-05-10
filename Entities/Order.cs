41d73ec61418dff7a76f3db6d9b93813246ae1e9
Luiz Soares <dev.soares@outlook.com>
2019-05-10 18:05:53 -0300

    Finished application

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -1,10 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ComercialApp.Entities.Enums;
+
 
 namespace ComercialApp.Entities
 {
     class Order
     {
+        public Guid Id { get; private set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; private set; }
+        public List<OrderItem> Items { get; private set; } = new List<OrderItem>();
+
+        public Order(OrderStatus status, Client client)
+        {
+            Id = Guid.NewGuid();
+            Moment = DateTime.Now;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0f;
+            foreach(OrderItem item in Items)
+            {
+                sum += item.Price * item.Quantity;
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Order ID: ");
+            sb.AppendLine(Id.ToString());
+            sb.Append("Order Moment: ");
+            sb.AppendLine(Moment.ToString("mm/dd/yyyy HH:mm:ss"));
+            sb.Append("Order Status: ");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Client: ");
+            sb.AppendLine(Client.ToString());
+            sb.AppendLine("Order items: ");
+            foreach(OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.Append("Total price: ");
+            sb.AppendLine(this.Total().ToString("F2"));
+
+            return sb.ToString();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ComercialApp.Entities;
+using ComercialApp.Entities.Enums;
+using System;
 
 namespace ComercialApp
 {
@@ -6,7 +8,48 @@ namespace ComercialApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte trava = 1;
+            while (trava != 0)
+            {
+                Console.WriteLine("Enter client data:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Birth date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Enter order data:");
+                Console.Write("Status: ");
+                OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+                Console.Write("How many items to this order? ");
+                int n = int.Parse( Console.ReadLine());
+
+                Client client = new Client(name, email, date);
+                Order order = new Order(status, client);
+
+                for(int i=1; i<=n; i++)
+                {
+                    Console.WriteLine($"Enter #{i} item data:");
+                    Console.Write("Enter product name: ");
+                    string productName = Console.ReadLine();
+                    Console.Write("Enter product price: ");
+                    double productPrice = double.Parse(Console.ReadLine());
+                    Console.Write("Quantity: ");
+                    int productQuantity = int.Parse(Console.ReadLine());
+                    Product prod = new Product(productName, productPrice);
+                    OrderItem orderItem = new OrderItem(productQuantity, productPrice, prod);
+                    order.AddItem(orderItem);
+                }
+
+                Console.WriteLine("Order Sumary:");
+                Console.WriteLine(order);
+
+
+
+                Console.WriteLine("Type 0 to quit..");
+                trava = byte.Parse(Console.ReadLine());
+                Console.Clear();
+            }
         }
     }
 }
